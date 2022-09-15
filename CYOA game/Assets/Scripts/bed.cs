a1141d5b1a1606396b7d4965cb5a2654f6083105
ev427 <111007327+ev427@users.noreply.github.com>
2022-09-15 11:04:26 -0700

    update

---
+++ CYOA game/Assets/Scripts/bed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bed : MonoBehaviour
+{
+    public void OnPress()
+    {
+    SceneManager.LoadScene(3);
+    }
+}
---
+++ CYOA game/Assets/Scripts/forest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class forest : MonoBehaviour
+{
+    public void OnPress()
+    {
+    SceneManager.LoadScene(6);
+    }
+}
