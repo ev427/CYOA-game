86f0b48f9b90d21183526ac2766d30300afb8d7b
ev427 <111007327+ev427@users.noreply.github.com>
2022-09-19 11:23:59 -0700

    update

---
+++ CYOA game/Assets/Scripts/kiss.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class kiss : MonoBehaviour
+{
+    public void OnPress()
+    {
+    SceneManager.LoadScene(9);
+    }
+}
---
+++ CYOA game/Assets/Scripts/stap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class stap : MonoBehaviour
+{
+        public void OnPress()
+    {
+    SceneManager.LoadScene(8);
+    }
+}
